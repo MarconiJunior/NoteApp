1f23df1580f8ca9e9a0407a53970bcdd5e506244
MarconiJunior <junior@brumafe.com>
2025-02-22 12:01:28 -0300

    chore : update compose and related dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,8 +61,8 @@ android {
 }
 
 dependencies {
-    implementation("androidx.compose.runtime:runtime-livedata:1.7.6")
-    val composeVersion = "1.7.6"
+    implementation("androidx.compose.runtime:runtime-livedata:1.7.8")
+    val composeVersion = "1.7.8"
 
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
@@ -84,7 +84,7 @@ dependencies {
     // Compose dependencies
 
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
-    implementation("androidx.navigation:navigation-compose:2.8.5")
+    implementation("androidx.navigation:navigation-compose:2.8.7")
     implementation("androidx.compose.material:material-icons-extended:$composeVersion")
     implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
 
